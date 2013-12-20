35d4c0d0493a606be111e2f907cb916435bbb2da
singro <isingro@gmail.com>
2013-12-20 11:07:54 +0800

    Initial Commit

---
+++ SCTabViewController/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  SCTabViewController
+//
+//  Created by Singro on 12/20/13.
+//  Copyright (c) 2013 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SCTabViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCTabViewController
+//
+//  Created by Singro on 12/20/13.
+//  Copyright (c) 2013 Singro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
